595709cd6907ffbed6d88a4261124cd9943e91d8
AniSmart-86 <anismart124@gmail.com>
2024-03-31 22:49:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+
+const emojiData = [
+  {
+    id: 1,
+    name: "smiling happy, ",
+    symbol: ["😀", "😁", "😂", "🤣", "😃",  
+    "😄", "😅", "😆", "😉", "😊", 
+    "😋", "😎", "😍","😘","🥰","😗","😙","😚","🤗","🤩","🤔","🤨","😐","😑","😶","😶","🙄","😏","😣","😥",'😮','🤐','😪','😫','🥱','😴','😌','😛','😜','😝','🤤','😒','😓','😔','😕','🙃','🤑','😲','☹','🙁','😖','😞','😟','😤','😢','😭','😦','😧','😨','😩','🤯','😬','😰','😱','🥵','🥶','😳','🤪','😵','🥴','😠','😡','🤬','😷','🤒','🤕'],
+  },
+
+   {
+    id: 2,
+    name: "people",
+    symbol: ['👩','👩','👨','🧑','🧑','🧑','👧','👦','🧒','👶','👵','👴','🧓','👩‍🦰','👨‍🦰','👩‍🦱','👨‍🦱','👩‍🦲','👨‍🦲','🕵️‍♀️','👮‍♂️','👼','🧔','👲','👩‍🦳','👨‍🦳','👨‍💻','👩‍💻','👩‍🎨','🧜‍♂️','🧛‍♀️','💁‍♀️',],
+  },
+   {
+    id: 3,
+    name: "objects",
+    symbol: ['🎈','🎆','🎇','🧨','✨','🎉','🎊','🎃','🎄','🎋','🎍','🎎','🎏','🎐','🎑','🧧',],
+  },
+   {
+    id: 4,
+    name: "food",
+    symbol: ['🍕🍔🍟🌭🍿🧂🥓🥚🥯🥨🥐🥞🧇☕🍾🌽🍏🥂🍻🍺🍹🍸🍉🍊🍋🍌🌲🌱☘🥀🌷🌻🌼🌺'],
+  },
+
+  {
+    id: 5,
+    name: "transportation",
+    symbol: ["🚗🚓🚕🚕🚙🚌🚐🚎🚑🚒🚛🚜🚔🚖✈🪂🛩⛽🚘🚔🛰"]
+  },
+
+  {
+    id: 5,
+    name: "love",
+    symbol: ["❤🧡💛💛💚💙💜💜🖤💖💗💓💞❣💔🤍💘💝💟💌💢💥💤💦"]
+  }
+
+];
+
+
+
+const App = () => {
+
+
+
+const [searchTerm, setSearchTerm] = useState("");
+
+const HandleSearch = (e) =>{
+setSearchTerm(e.target.value);
+};
+
+const HandleButtonSearch = () =>{
+setSearchTerm("");
+}
+
+const filteredEmojis = emojiData.filter((emoji) => {
+    const emojiName = emoji.name.toLocaleLowerCase();
+    return searchTerm && emojiName.startsWith(searchTerm.toLowerCase());
+});
+
+//  setSearchTerm(filteredEmojis)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi! Search and copy your favourite Emojis</h1>
+      <p>Search something like smiling, love, objeccts and transportation💃🎆</p>
+
+      <div className='form'>
+    <input
+      type="text"
+      placeholder="Search for an emoji..."
+      value={searchTerm}
+      onChange={HandleSearch}
+    />
+    <button onClick={HandleButtonSearch}>Search</button>
+    
+    <div className='filter'>
+       {filteredEmojis.map((emoji) => emoji.symbol)}
+      </div>
+  </div>
+  <h3>Created by Ani Emmanuel</h3>
     </div>
   );
 }
